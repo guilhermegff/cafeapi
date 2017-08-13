d5c1f42a112240fc10a367d77218a71e665a00a2
guilhermegff <triclorotolueno@hotmail.com>
2017-08-12 22:58:35 -0700

    Commit 7

--- cafepi/urls.py
+++ cafepi/urls.py
@@ -21,3 +21,8 @@ urlpatterns = [
     url(r'^accounts/', include('django.contrib.auth.urls')),
     url(r'^', include('cafeapi.urls')),
 ]
+
+from django.conf import settings
+from django.conf.urls.static import static
+
+urlpatterns+= static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
